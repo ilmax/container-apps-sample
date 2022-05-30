2d27722487099a84cef8d176c895a6f8cc563798
Massimiliano Donini <massimiliano.donini@gmail.com>
2022-05-30 17:44:01 +0200

    Add github action

--- infra/main.tf
+++ infra/main.tf
@@ -10,4 +10,8 @@ resource "azurerm_resource_group" "aca-test-rg" {
   name     = "azure-container-app-test"
   location = var.location
   tags     = local.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
