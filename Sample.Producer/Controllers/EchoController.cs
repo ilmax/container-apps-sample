e5a727a0c38734679239380ee25ba3043c2dfae6
Massimiliano Donini <massimiliano.donini@gmail.com>
2022-06-23 08:49:04 +0200

    Add echo controller

---
+++ Sample.Producer/Controllers/EchoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Producer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EchoController : ControllerBase
+{
+    [HttpGet("ping")]
+    public ActionResult Ping() => Ok("pong");
+}
